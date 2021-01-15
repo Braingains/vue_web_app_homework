eea05d7f9fd695a40bfeec05497b4c3b9c64ffa9
Ewan Brain <ewan.brain@gmail.com>
2021-01-15 15:36:10 +0000

    fetching api

---
+++ pokedex_app/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+  <p>hello</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data (){
+    return { 
+      pokemon: []
+  }
+  },
+  components: {
+  },
+  mounted(){
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+    .then(res => res.json())
+    .then(pokemon => this.pokemon = pokemon);
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
